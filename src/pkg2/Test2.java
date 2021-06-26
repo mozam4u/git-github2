e43bd4ca8fce18599bb57a0f355ea01dd1432388
mozammil <mozammilh11@gmail.com>
2021-06-26 17:14:16 +0530

    pkg2 added

---
+++ src/pkg2/Test2.java
@@ -0,0 +1,16 @@
+package pkg2;
+
+import java.util.ArrayList;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+
+
+		ArrayList<Integer> ar = new ArrayList<>();
+		
+		ar.add(2);
+		ar.add(5);
+	}
+
+}
